a68b41aecc55702aba4d4e5823f2de8f5608e401
Christoph Kahl <26385576+kanonet@users.noreply.github.com>
2022-03-02 04:15:24 +0100

    add more camera parameters

--- modules/input/webcam_input.py
+++ modules/input/webcam_input.py
@@ -1,6 +1,7 @@
 import time
 from threading import Thread, Lock, Event
 from platform import system as pf_system
+from math import log2, ceil
 import cv2
 
 from lib.auxiliary import get_time_ms
@@ -40,22 +41,56 @@ class WebcamInput(ModuleBase):
         self.camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, int(config["width"]))
         self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, int(config["height"]))
+        if config["autoexposure"]:
+            self.camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)  # has no effect on windows
+        elif config["exposure"] is not None and config["exposure"] > 0:
+            self.camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # manual exposure mode
+            # setting camera exposure timing is different on Linux and Windows
+            # check: https://www.kurokesu.com/main/2020/05/22/uvc-camera-exposure-timing-in-opencv/
+            if pf_system() == 'Windows':
+                exp = -ceil(log2(1000/config["exposure"]))
+            elif pf_system() == 'Linux':
+                exp = config["exposure"] * 10
+            self.camera.set(cv2.CAP_PROP_EXPOSURE, exp)
+        if config["gain"] is not None:
+            self.camera.set(cv2.CAP_PROP_GAIN, config["gain"])
+        if config["brightness"] is not None:
+            self.camera.set(cv2.CAP_PROP_BRIGHTNESS, config["brightness"])
+        if config["contrast"] is not None:
+            self.camera.set(cv2.CAP_PROP_CONTRAST, config["contrast"])
+        if config["autofocus"]:
+            self.camera.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+        elif config["focus"] is not None:
+            self.camera.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+            self.camera.set(cv2.CAP_PROP_FOCUS, config["focus"])
+        if config["zoom"] is not None:
+            self.camera.set(cv2.CAP_PROP_ZOOM, config["zoom"])
+        if config["win_props_dialog"]:
+            self.camera.set(cv2.CAP_PROP_SETTINGS, 1)
+
         self.scale = float(config["scale"])
         self.delay = float(config["delay"])
-
         self.intrinsics = WebcamInput.guessIntrinsics(self.scale * self.camera.get(cv2.CAP_PROP_FRAME_WIDTH),
                         self.scale * self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        print("Camera " + str(config["device"]) + " opened with:")
-        print("  fourcc:            " + str(self.camera.get(cv2.CAP_PROP_FOURCC)))
-        print("  width:             " + str(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)))
-        print("  height:            " + str(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        print("  fps:               " + str(self.camera.get(cv2.CAP_PROP_FPS)))
-        print("  buffersize:        " + str(self.camera.get(cv2.CAP_PROP_BUFFERSIZE)))
-        print("  scale factor:      " + str(self.scale))
-        print("  fx, fy, cx, cy:    " + str(self.intrinsics))
+        print("Camera", config["device"], "opened with:")
+        print("  fourcc:           ", self.camera.get(cv2.CAP_PROP_FOURCC))
+        print("  width:            ", self.camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print("  height:           ", self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        print("  fps:              ", self.camera.get(cv2.CAP_PROP_FPS))
+        print("  buffersize:       ", self.camera.get(cv2.CAP_PROP_BUFFERSIZE))
+        print("  auto exposure:    ", self.camera.get(cv2.CAP_PROP_AUTO_EXPOSURE))
+        print("  exposure:         ", self.camera.get(cv2.CAP_PROP_EXPOSURE))
+        print("  gain:             ", self.camera.get(cv2.CAP_PROP_GAIN))
+        print("  brightness:       ", self.camera.get(cv2.CAP_PROP_BRIGHTNESS))
+        print("  contrast:         ", self.camera.get(cv2.CAP_PROP_CONTRAST))
+        print("  autofocus:        ", self.camera.get(cv2.CAP_PROP_AUTOFOCUS))
+        print("  focus:            ", self.camera.get(cv2.CAP_PROP_FOCUS))
+        print("  zoom:             ", self.camera.get(cv2.CAP_PROP_ZOOM))
+        print("  scale factor:     ", self.scale)
+        print("  fx, fy, cx, cy:   ", self.intrinsics)
         print("")
-        print("Sending delay:" + str(self.delay))
+        print("Sending delay:", self.delay)
 
         if bool(config["capture_thread"]):
             print("Using async capture thread.")
